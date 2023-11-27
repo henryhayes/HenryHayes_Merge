4cedc8a5b6951bad98cebf66445c3dc44e7fbc50
Henry Hayes <henryhayesuk@gmail.com>
2023-11-27 22:34:01 +0000

    Updated composer.json with plugin.

--- tests/app/code/community/HenryHayes/Merge/Model/Merge/AbstractTest.php
+++ tests/app/code/community/HenryHayes/Merge/Model/Merge/AbstractTest.php
@@ -3,9 +3,14 @@ use PHPUnit\Framework\TestCase;
 
 final class HenryHayes_Merge_Model_Merge_AbstractTest extends TestCase
 {
-    public function testConcreteMethod(): void
+    public function testConcreteMethod()
     {
-        $stub = $this->getMockForAbstractClass('HenryHayes_Merge_Model_Merge_Abstract');
+        $stub = $this->getMockForAbstractClass(
+            'HenryHayes_Merge_Model_Merge_Abstract',
+            [] /* arguments */,
+            '' /* mockClassName */,
+            false /* callOriginalConstructor */
+        );
 
         $this->assertInstanceOf('HenryHayes_Merge_Model_Merge_Abstract', $stub);
     }
